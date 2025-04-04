b7ea4c4ea6cb4752728e3457ae11b69d9774029e
Khaled.Eid <Khaled.Eid@souhoola.com>
2025-04-04 06:55:46 +0200

    refactoring

--- src/main/kotlin/main/isIpv4Valid.kt
+++ src/main/kotlin/main/isIpv4Valid.kt
@@ -1,5 +1,15 @@
 package main
 
-fun isIpv4Valid(iPVersionNumber : String): Boolean{
-    return  false
+fun isIpv4Valid(iPVersionNumber: String): Boolean {
+    val ipNumbers = iPVersionNumber.split('.')
+    if (ipNumbers.size == 4) {
+       return ipNumbers.all {
+                   it.isNotEmpty()&& it.isNotBlank() && it.toIntOrNull() in 0..254 && (it == "0"|| it[0] != '0')
+                }
+
+
+
+    }
+
+    return false
 }
